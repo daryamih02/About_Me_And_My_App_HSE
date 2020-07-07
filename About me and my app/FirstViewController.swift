dd986b7ddef06eb058fa7fb3e6eaf9cabdd66517
Daryamih02 <daryamih02@gmail.com>
2020-07-07 13:17:24 +0300

    Initial Commit

---
+++ About me and my app/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  About me and my app
+//
+//  Created by MacBook Air on 07/07/2020.
+//  Copyright © 2020 MacBook Air. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ About me and my app/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  About me and my app
+//
+//  Created by MacBook Air on 07/07/2020.
+//  Copyright © 2020 MacBook Air. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
